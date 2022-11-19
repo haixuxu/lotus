1f87c64a7a0185739b7b00794d694ac5caceab4e
xuxihai <xuxihai123@gmail.com>
2022-11-19 17:14:33 +0800

    Initial Commit

---
+++ Lotus/LotusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotusApp.swift
+//  Lotus
+//
+//  Created by xuxihai on 2022/11/19.
+//
+
+import SwiftUI
+
+@main
+struct LotusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
